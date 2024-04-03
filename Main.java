e8ce13668f7290cd005c849d8b3923a3312d9cb9
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-03 22:45:55 +0900

    Javaの学習開始

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) { 
+    System.out.println("ちゃす");
+    System.out.println("ちゃす");
+    System.out.println("ちゃす");
+    System.out.println("ちゃす");
+    System.out.println("ちゃす");
+    System.out.println("31" + "31");
+    int x;
+    x = 6;
+    System.out.println(x * x);
+  }
+}
