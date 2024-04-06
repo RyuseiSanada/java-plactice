54fffb42f47e5fc26088af88d0f902ec3efe2920
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-06 09:32:54 +0900

    クラス

---
+++ section6_class/Main.java
@@ -0,0 +1,13 @@
+import comment.*;
+
+public class Main {
+  public static void main(String[] args) {
+    //前半
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+
+    //後半
+    Kouhan.callDeae();
+    Kouhan.showMondokoro();
+  }
+}
