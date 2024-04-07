f30c5612b508dff343137dff4f3a421c1a02c60e
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-07 11:17:09 +0900

    色々の変更

--- section6_class/comment/Kouhan.java
+++ section6_class/comment/Kouhan.java
@@ -7,6 +7,10 @@ public class Kouhan {
   public static void showMondokoro() {
     System.out.println("飛車さん、角さん。もういいでしょう。");
     System.out.println("この紋所が目に入らぬか！");
+    try {
+      Thread.sleep(3000); // 10秒(1万ミリ秒)間だけ処理を止める
+    } catch (InterruptedException e) {
+    }
     Zenhan.doTogame();
   }
 }
