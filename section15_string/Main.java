dc2cdbb35cafcebd7d0102bbc16eb3eba233a113
RyuseiSanada <sanadaryuusei@gmail.com>
2024-04-10 08:13:31 +0900

    正規表現

--- section15_string/Main.java
+++ section15_string/Main.java
@@ -1,19 +1,39 @@
 public class Main {
 
-  public static String stringConnect() {
+  public static void stringConnect() {
     StringBuilder sb = new StringBuilder();
     for (int i = 1; i <= 100; i++) {
-      sb.append(i + ",");
+      sb.append(i);
+      sb.append(",");
     }
-    return sb.toString();
+    String s = sb.toString();
+    String[] a = s.split(",");
+    System.out.println(a);
+  }
+
+  public static void makeFileAddress(String folder, String file) {
+    if (folder.matches("c:¥\\w+¥?") && file.matches("\\w+\\.\\w+")) {
+      StringBuilder sb = new StringBuilder();
+      if(folder.endsWith("¥")) {
+        System.out.println(sb.append(folder).append(file).toString()); 
+      } 
+      else {
+        System.out.println(sb.append(folder).append("¥").append(file).toString()); 
+      }
+    } 
+    else {
+      System.out.println("フォルダー、またはファイル名が不正です");
+    }
   }
 
   public static void main(String[] args) {
-    String s = stringConnect();
-    String[] a = s.split(",");
-    for (String num : a) {
-      System.out.println(num);
-    }
+    stringConnect();
 
+    makeFileAddress("c:¥rightFolder¥", "sanada.tex");
+    makeFileAddress("c:¥rightFolder", "sanachan.tex");
   }
+
+
+
+
 }
